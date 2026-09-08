54f0cf23954d5d9ff906c460c2ef1de032e0555c
Aren Vox <airanluo@gmail.com>
2026-09-09 00:55:30 +0800

    fix: format embedded update trust key as valid raw C# text

--- src/NovaClip.App/Services/SignedUpdateTrustPolicy.cs
+++ src/NovaClip.App/Services/SignedUpdateTrustPolicy.cs
@@ -3,8 +3,10 @@ namespace NovaClip.App;
 internal static class SignedUpdateTrustPolicy
 {
     public const string KeyId = "novaclip-beta7-2026";
+
     // The corresponding private key is never stored in the repository. Release signing must use a protected CI secret.
-    public const string PublicKeyPem = """-----BEGIN PUBLIC KEY-----
+    public const string PublicKeyPem = """
+-----BEGIN PUBLIC KEY-----
 MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA1k8Ll2CwX8sWPASf9dx6
 S8VDahjgO+wnXRQqX3EUa2h001fs8DnFMMmhQBSBPFDOYxYsRUnBFWmla7/hYyCV
 gIpYcFaGQedokoXXyeW8bB1Ua86RnvFghF6D2HHB5euNhhhw+YHPtERP7zEy21BZ
@@ -12,5 +14,6 @@ X/TC/bSNdfWPHVR6Q+QIIdznb65odc28+bRNvwTVzmQrIBzFrYh3mlncyFmoztXq
 mAGcyBXUcS09Mx9jEfGEKMTNTIkNWKUSWmHl9xRCjpt+fHTfyOWbk1MB2jGwhI3+
 7dWNENVOTxI7E6Lv+olL4D+ggJfzpoiTDRmLYh+xeVUlDIeoMke75B/nJKCzimyr
 6wIDAQAB
------END PUBLIC KEY-----""";
+-----END PUBLIC KEY-----
+""";
 }
